314ebe087b98f578467baaa4770cadbb44f23d57
Ferucci <development.first1@gmail.com>
2024-10-06 17:32:53 +0300

    fixed block filter

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,4 @@ import { toggleDropdown } from "./module/dropdown.js";
 const sidebarLink = document.querySelectorAll('.sidebar__item[data-dropdown] > .sidebar__link');
 toggleDropdown(sidebarLink);
 
-
-const selectOptions = {
-  placeholder: 'Фильтры',
-  selectedId: '1',
-  data: [
-    { id: '1', value: 'Услуги' },
-    { id: '2', value: 'Продажи' },
-    { id: '3', value: 'Расходники' },
-    { id: '4', value: 'Покупки' },
-  ],
-  onSelect(item) {
-    // console.log('Выбранный элемент: ', item)
-  }
-}
-
-const select = new Select('#select', selectOptions);
+Select()
