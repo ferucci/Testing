3437b25bde9251f31da90a54c140e8a64263589b
Ferucci <development.first1@gmail.com>
2024-11-22 14:51:43 +0300

    change name file dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { Select } from "./module/filter/filter-select.js";
 import { toggleDropdown } from "./module/aside-dropdown.js";
-import { changeDate } from "./module/filter/index.js";
+import { filterByDate } from "./module/filter/index.js";
 import { vars } from "./module/vars.js";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const { sidebarLinks } = vars;
 
   toggleDropdown(sidebarLinks);
-  changeDate()
+  filterByDate()
   Select()
 
 })
