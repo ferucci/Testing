c60809114c2753cce26c07e643c1f706e87b637e
Ferucci <development.first1@gmail.com>
2024-11-22 14:48:57 +0300

    change name file dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import { Select } from "./module/filter-select.js";
+import { Select } from "./module/filter/filter-select.js";
 import { toggleDropdown } from "./module/aside-dropdown.js";
-import { changeDate } from "./module/filter-date.js";
+import { changeDate } from "./module/filter/index.js";
 import { vars } from "./module/vars.js";
 
 window.addEventListener("DOMContentLoaded", () => {
